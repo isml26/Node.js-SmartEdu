0362bc6028cb772130fe8c6d7d440f69a28781b6
unknown <iguven2697@gmail.com>
2021-06-03 17:08:01 +0300

    Lesson07

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ const dotenv = require("dotenv")
 const pageRoute = require('./routes/pageRoute')
 const courseRoute = require("./routes/courseRoute");
 const app = express();
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({
+  extended: true
+})) // for parsing application/x-www-form-urlencoded
 dotenv.config();
 //Template Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 //Middleware
 app.use(express.static('public'));
 
 
 const PORT = 3000;
 
-mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
 const connection = mongoose.connection;
-connection.once("open",()=>{
+connection.once("open", () => {
   console.log("Connected Successfully");
 });
 
@@ -23,10 +32,10 @@ connection.once("open",()=>{
 
 //Routes
 app.use('/', pageRoute);
-app.use('/courses',courseRoute);
+app.use('/courses', courseRoute);
 app.use('/about', pageRoute);
 
 
 app.listen(PORT, (req, res) => {
-    console.log(`App started on ${PORT}`);
+  console.log(`App started on ${PORT}`);
 });
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,8 @@
 const Course = require("../models/Course");
 
 exports.createCourse = async (req, res) => {
-    const course = await Course.create(req.body);
     try {
+    const course = await Course.create(req.body);
         res.status(201).json({
             status: 'success',
             course
@@ -14,4 +14,19 @@ exports.createCourse = async (req, res) => {
             error
         })
     }
-}
+};
+exports.getAllCourses = async (req, res) => {
+    try {
+    const courses = await Course.find();
+        res.status(200).render('courses',{
+            courses,
+            page_name:"courses"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            status: 'failed',
+            error
+        })
+    }
+};
