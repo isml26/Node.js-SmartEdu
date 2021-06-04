b78f5c2b9da910b82df34d9dca944490f79b14f5
unknown <iguven2697@gmail.com>
2021-06-04 12:44:40 +0300

    Lesson10-Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
@@ -14,4 +14,25 @@ exports.createUser = async (req, res) => {
             error
         })
     }
-}
+};
+exports.loginUser = async (req, res) => {
+    try {
+        const {email,password} = req.body;
+        await User.findOne({email},(err,user)=>{
+            if(user){
+                bcrypt.compare(password,user.password,(err,same)=>{
+                    if(same){
+                        //USER SESSION
+                        res.status(200).send('You are logged in');
+                    }
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            status: 'failed',
+            error
+        })
+    }
+};
