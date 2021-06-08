784522a0f33f19782722a278c05203857325926d
unknown <iguven2697@gmail.com>
2021-06-08 12:52:23 +0300

    17 Course-Teacher Relationship

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,14 @@
 const Course = require("../models/Course");
 const Category = require("../models/Category");
+const User = require("../models/User");
 exports.createCourse = async (req, res) => {
     try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+        name:req.body.name,
+        description:req.body.description,
+        category:req.body.category,
+        user:req.session.userID,
+    });
         res.status(201).redirect('/courses');
     } catch (error) {
         console.log(error);
@@ -39,7 +45,7 @@ exports.getAllCourses = async (req, res) => {
 };
 exports.getCourse = async (req, res) => {
     try {
-    const course = await Course.findOne({slug:req.params.slug})
+    const course = await Course.findOne({slug:req.params.slug}).populate('user')
         res.status(200).render('course',{
             course,
             page_name:"courses"
